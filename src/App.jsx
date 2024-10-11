45817be47e16198431b4c5d7ff14cf70201853e7
Armand Soto <armand.e.soto@gmail.com>
2024-10-11 13:43:04 -0400

    Added Hero Component and work img for (possible) carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
     <div className="">
       <Header />
       <Navbar />
+      <Hero />
 
     </div>
   )
